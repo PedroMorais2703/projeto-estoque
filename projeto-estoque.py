9e2f561ad4c2cc3c1309a4826ab9aaddde874da4
PedroMorais <95058545+PedroMorais2703@users.noreply.github.com>
2023-07-19 22:17:26 -0300

    Update projeto-estoque.py

--- projeto-estoque.py
+++ projeto-estoque.py
@@ -1,4 +1,4 @@
-lista = ["celular - valor: 900,00 - quantidade - 3", "tablet - valor: 700,00 - quantidade - 5"]
+lista = ["celular", "900.00, 5", "tablet", "800.00, 5"]
 
 while True:
     print("===== MENU DE OPÇÕES =====")
@@ -13,16 +13,33 @@ while True:
         item = str(input("Digite item desejado: "))
         valor = float(input("Digite o valor desejado: "))
         quantidade = int(input("Digite a quantidade desejada: "))
-        lista.extend([item, valor, quantidade])
+        valor_quantidade = [valor , quantidade]
+        lista.append(item)
+        lista.append(valor_quantidade)
         # Faça algo relacionado à opção 1
         
     elif escolha == "2":
-        print("Você escolheu a opção 2.")
-        # Faça algo relacionado à opção 2
+        item_atualizar = input("item desejado")
+        
+        indice = lista.index(item_atualizar)
+        novo_indice = indice + 1
+        novo_valor = input("digite o valor desejado")
+        valor_deletar = novo_indice
+        for i in range(len(lista)):
+            if lista[novo_indice] == item_atualizar:
+                lista[novo_indice] = novo_valor
+                break
+        
+        del lista[valor_deletar]
+        
+        
+        
+        
+        lista.insert(novo_indice, novo_valor)
         
     elif escolha == "3":
         print(lista)
-        # Faça algo relacionado à opção 3
+        
         
     elif escolha == "0":
         print("Saindo do programa...")
